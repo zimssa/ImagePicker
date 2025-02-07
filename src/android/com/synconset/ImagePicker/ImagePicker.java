68c76bf6d6fae00fd9dadefc9757d9add7b0bd91
허정민 <jmhur@zimssa.com>
2025-02-07 11:48:33 +0900

    fix: 10이하 버전 external storage 권한 물어보기

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -52,13 +52,31 @@ public class ImagePicker extends CordovaPlugin {
             throws JSONException {
         this.callbackContext = callbackContext;
 
-        if (ACTION_GET_PICTURES.equals(action)) {
+        if (ACTION_HAS_READ_PERMISSION.equals(action)) {
+            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, hasReadPermission()));
+            return true;
+
+        } else if (ACTION_REQUEST_READ_PERMISSION.equals(action)) {
+            requestReadPermission();
+            return true;
+
+        } else if (ACTION_GET_PICTURES.equals(action)) {
             final JSONObject params = args.getJSONObject(0);
             imagePickerIntent = getImagePickerIntent(params);
-            cordova.startActivityForResult(this, imagePickerIntent, 0);
+
+            if (hasReadPermission()) {
+                cordova.startActivityForResult(this, imagePickerIntent, 0);
+            } else {
+                if (getPreference(PERMISSION_REQUESTED) == false) {
+                    requestReadPermission();
+                    // callbackContext.success();
+                } else {
+                    callbackContext
+                            .error("저장소 접근이 제한되었습니다. [설정 > 앱 > " + getApplicationName() + "]에서 저장소 접근 권한을 허용해 주세요.");
+                }
+            }
             return true;
         }
-
         return false;
     }
 
@@ -106,6 +124,27 @@ public class ImagePicker extends CordovaPlugin {
         return imagePickerIntent;
     }
 
+    @SuppressLint("InlinedApi")
+    private boolean hasReadPermission() {
+        String permission = Manifest.permission.READ_EXTERNAL_STORAGE;
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.Q) {
+            return cordova.hasPermission(permission);
+        }
+        return true;
+    }
+
+    @SuppressLint("InlinedApi")
+    private void requestReadPermission() {
+        if (!hasReadPermission()) {
+            String[] permissions = { Manifest.permission.READ_EXTERNAL_STORAGE };
+
+            setPreference(PERMISSION_REQUESTED, true);
+            cordova.requestPermissions(this,
+                    PERMISSION_REQUEST_CODE,
+                    permissions);
+        }
+    }
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK && data != null) {
             int sync = data.getIntExtra("bigdata:synccode", -1);
@@ -129,11 +168,36 @@ public class ImagePicker extends CordovaPlugin {
         }
     }
 
+    /**
+     * Choosing a picture launches another Activity, so we need to implement the
+     * save/restore APIs to handle the case where the CordovaActivity is killed by
+     * the OS
+     * before we get the launched Activity's result.
+     *
+     * @see ://cordova.apache.org/docs/en/dev/guide/platforms/android/plugin.html#launching-other-activities
+     */
+    public void onRestoreStateForActivityResult(Bundle state, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
+
+    @Override
+    public void onRequestPermissionResult(int requestCode,
+            String[] permissions,
+            int[] grantResults) throws JSONException {
+
+        // For now we just have one permission, so things can be kept simple...
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            cordova.startActivityForResult(this, imagePickerIntent, 0);
+        } else {
+            // Tell the JS layer that something went wrong...
+            callbackContext.error("저장소 접근이 제한되었습니다. [설정 > 앱 > " + getApplicationName() + "]에서 저장소 접근 권한을 허용해 주세요");
+        }
+    }
+
     private String getApplicationName() {
         Context context = cordovaActivity.getApplicationContext();
         ApplicationInfo applicationInfo = context.getApplicationInfo();
         int stringId = applicationInfo.labelRes;
         return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
     }
-
 }
